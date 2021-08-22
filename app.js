a6cb51bc719994b7f5299e53c9aba8b3f646dda8
Robert Johnston <anaerin@gmail.com>
2021-08-22 03:49:32 -0600

    Implement a prettier UI using blessed and blessed-contrib

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 import config from './config.js';
 import tmi from 'tmi.js';
 import RollingArray from './lib/rollingArray.js';
+import UI from './lib/console.js';
+
+const ui = new UI();
 
 const args = new Map();
 const argv = process.argv.slice(2);
@@ -44,116 +47,122 @@ if (!config.oAuthToken) {
 
 const rollingArray = new RollingArray({ pruneFrequency: config.pruneFrequency, keepFor: config.keepFor, threshold: config.threshold, minLength: config.minLength } );
 
+ui.setStatus("Disconnected.");
 const client = new tmi.Client(connectionOptions);
 
 let timeout = false;
 
 client.on('ban', (channel, username, reason) => {
-	console.info(`${channel}: User ${username} banned for ${reason}`);
+	ui.log(`${channel}: User ${username} banned for ${reason}`);
 });
 
 client.on('clearchat', (channel) => {
-	console.info(`${channel}: Chat was cleared`);
+	ui.log(`${channel}: Chat was cleared`);
 });
 
 client.on('connected', (address, port) => {
-	console.info(`SERVER: Connected to ${address}:${port}`);
+	ui.log(`SERVER: Connected to ${address}:${port}`);
+	ui.setStatus(`Connected to ${address}:${port}`);
 });
 
 client.on('connecting', (address, port) => {
-	console.info(`SERVER: Connecting to ${address}:${port}`);
+	ui.log(`SERVER: Connecting to ${address}:${port}`);
+	ui.setStatus(`Connecting to ${address}:${port}...`);
 });
 
 client.on('disconnected', (reason) => {
-	console.info(`SERVER: Disconnected from server (${reason})`);
+	ui.log(`SERVER: Disconnected from server (${reason})`);
+	ui.setStatus(`Disconnected`);
 });
 
 client.on('emoteonly', (channel, enabled) => {
-	console.info(`${channel}: Emote only mode ${enabled?"enabled":"disabled"}`);
+	ui.log(`${channel}: Emote only mode ${enabled?"enabled":"disabled"}`);
 });
 
 client.on('followersonly', (channel, enabled, length) => {
-	console.info(`${channel}: Followers only mode ${enabled?"enabled, " + length:"disabled"}`);
+	ui.log(`${channel}: Followers only mode ${enabled?"enabled, " + length:"disabled"}`);
 });
 
 client.on('hosted', (channel, username, viewers, autohost) => {
-	console.info(`${channel}: Now being ${autohost?"auto":""}hosted by ${username} with ${viewers} viewers`);
+	ui.log(`${channel}: Now being ${autohost?"auto":""}hosted by ${username} with ${viewers} viewers`);
 });
 
 client.on('hosting', (channel, target, viewers) => {
-	console.info(`${channel}: Now hosting ${target} for ${viewers} viewers`);
+	ui.log(`${channel}: Now hosting ${target} for ${viewers} viewers`);
 });
 
 client.on('join', (channel, username, self) => {
-	console.info(`${channel}: ${self?"We":username} joined channel ${channel}`);
+	ui.log(`${channel}: ${self?"We":username} joined channel ${channel}`);
 });
 
 client.on('logon', () => {
-	console.info(`SERVER: Connected, logging in`);
+	ui.log(`SERVER: Connected, logging in`);
+	ui.setStatus(`Connected, logging in...`);
 });
 
 client.on('messagedeleted', (channel, username, deletedMessage, userState) => {
-	console.info(`${channel}: Message ${userState["target-msg-id"]} deleted by ${username}: '${deletedMessage}'`);
+	ui.log(`${channel}: Message ${userState["target-msg-id"]} deleted by ${username}: '${deletedMessage}'`);
 });
 
 client.on('notice', (channel, msgid, message) => {
-	console.info(`${channel}: NOTICE - ${msgid} (${message})`);
+	ui.log(`${channel}: NOTICE - ${msgid} (${message})`);
 });
 
 client.on('part', (channel, username, self) => {
-	console.info(`${channel}: ${self?"We":username} left channel ${channel}`);
+	ui.log(`${channel}: ${self?"We":username} left channel ${channel}`);
 });
 
 client.on('ping', () => {
-	console.info(`SERVER: Received keepalive message`);
+	ui.log(`SERVER: Received keepalive message`);
 });
 
 client.on('pong', (latency) => {
-	console.info(`SERVER: Round-Trip Time ${latency}s`);
+	ui.setLatency(latency);
 });
 
 client.on('r9kbeta', (channel, enabled) => {
-	console.info(`${channel}, r9kBeta mode ${enabled?"enabled":"disabled"}`);
+	ui.log(`${channel}, r9kBeta mode ${enabled?"enabled":"disabled"}`);
 });
 
 client.on('raided', (channel, username, viewers) => {
-	console.info(`${channel}: Incoming raid from ${username} with ${viewers} viewers`);
+	ui.log(`${channel}: Incoming raid from ${username} with ${viewers} viewers`);
 });
 
 client.on('reconnect', () => {
-	console.info(`SERVER: Reconnecting`);
+	ui.log(`SERVER: Reconnecting`);
+	ui.setStatus(`Reconnecting...`);
 });
 
 client.on('resub', (channel, username, months, message, userstate, method) => {
-	console.info(`${channel}: Resub from ${username}, ${userstate["msg-param-cumulative-months"]} months, ${userstate["msg-param-should-share-streak"]?months + " consecutively,":""} - ${message}`);
+	ui.log(`${channel}: Resub from ${username}, ${userstate["msg-param-cumulative-months"]} months, ${userstate["msg-param-should-share-streak"]?months + " consecutively,":""} - ${message}`);
 });
 
 client.on('slowmode', (channel, enabled, length) => {
-	console.info(`${channel}: Slow mode ${enabled?"enabled for "+length+" seconds":disabled}`);
+	ui.log(`${channel}: Slow mode ${enabled?"enabled for "+length+" seconds":disabled}`);
 });
 
 client.on('subgift', (channel, username, streak, recipient, method, userstate) => {
-	console.info(`${channel}: Sub Gift from ${username} to ${recipient}; ${userstate["msg-param-sender-count"]} gifts from them.`);
+	ui.log(`${channel}: Sub Gift from ${username} to ${recipient}; ${userstate["msg-param-sender-count"]} gifts from them.`);
 });
 
 client.on('submysterygift', (channel, username, number, method, userstate) => {
-	console.info(`${channel}: Sub Mystery Gift ${number} from ${userstate}; ${userstate["msg-param-sender-count"]} gifts from them.`);
+	ui.log(`${channel}: Sub Mystery Gift ${number} from ${userstate}; ${userstate["msg-param-sender-count"]} gifts from them.`);
 });
 
 client.on('subscribers', (channel, enabled) => {
-	console.info(`${channel}: Subscribers only mode ${enabled?"enabled":"disabled"}`);
+	ui.log(`${channel}: Subscribers only mode ${enabled?"enabled":"disabled"}`);
 });
 
 client.on('subscription', (channel, username, method, message, userstate) => {
-	console.info(`${channel}: Subscription from ${username} - ${message}`);
+	ui.log(`${channel}: Subscription from ${username} - ${message}`);
 });
 
 client.on('timeout', (channel, username, reason, duration) => {
-	console.info(`${channel}: Timeout for ${username} (${duration} seconds)`);
+	ui.log(`${channel}: Timeout for ${username} (${duration} seconds)`);
 });
 
 client.on('unhost', (channel, viewers) => {
-	console.info(`${channel}: Stopped hosting with ${viewers} viewers`);
+	ui.log(`${channel}: Stopped hosting with ${viewers} viewers`);
 });
 
 function decorateUsername(tags) {
@@ -169,15 +178,17 @@ function decorateUsername(tags) {
 let lockdown = false;
 let actedOnThisRaid = [];
 setInterval(() => {
-	if (lockdown && (lockdown + (raidCleanupDelay * 1000)) < Date.now()) {
-		console.info(`#${channelName}: RAID timeout passed, revoking lockdown`);
+	if (lockdown && (lockdown + (config.raidCleanupDelay * 1000)) < Date.now()) {
+		ui.log(`#${channelName}: RAID timeout passed, revoking lockdown`);
+		ui.setStatus(`Connected...`);
+		ui.clearRaiders();
 		if (!testMode) {
 			if (config.subsOnly) client.subscribersoff(channelName);
 			else client.followersonlyoff(channelName);
 			client.r9kbetaoff(channelName);
 		} else {
-			if (config.subsOnly) console.info(`#${channelName}: TESTMODE - would remove Subs only mode`);
-			else console.info(`#${channelName}: TESTMODE - would remove Followers only mode`);
+			if (config.subsOnly) ui.log(`#${channelName}: TESTMODE - would remove Subs only mode`);
+			else ui.log(`#${channelName}: TESTMODE - would remove Followers only mode`);
 		}
 		timeout = false;
 		lockdown = false;
@@ -189,16 +200,16 @@ client.on('message', (channel, tags, message, self) => {
 	const matchingMessages = rollingArray.likeRecently(message); // Get all the messages that are similar to this one.
 	switch(tags["message-type"]) {
 		case "action":
-			console.info(`${channel}: ${matchingMessages.length}/${config.similarMessageThreshold} * ${decorateUsername(tags)} ${message}`);
+			ui.log(`${channel}: ${matchingMessages.length}/${config.similarMessageThreshold} * ${decorateUsername(tags)} ${message}`);
 			break;
 		case "chat":
-			console.info(`${channel}: ${matchingMessages.length}/${config.similarMessageThreshold} <${decorateUsername(tags)}> ${message}`);
+			ui.log(`${channel}: ${matchingMessages.length}/${config.similarMessageThreshold} <${decorateUsername(tags)}> ${message}`);
 			break;
 		case "whisper":
-			console.info(`${channel}: ${matchingMessages.length}/${config.similarMessageThreshold} [${decorateUsername(tags)}] ${message}`);
+			ui.log(`${channel}: ${matchingMessages.length}/${config.similarMessageThreshold} [${decorateUsername(tags)}] ${message}`);
 			break;
 		default:
-			console.info(`${channel}: ${matchingMessages.length}/${config.similarMessageThreshold} ?${decorateUsername(tags)}? ${message}`);
+			ui.log(`${channel}: ${matchingMessages.length}/${config.similarMessageThreshold} ?${decorateUsername(tags)}? ${message}`);
 			break;
 	}
 	if(self) return;
@@ -208,14 +219,15 @@ client.on('message', (channel, tags, message, self) => {
 		let messageIDs = [];
 		let userIDs = [];
 		if (!lockdown) {
-			console.warn(`${channel}: RAID DETECTED, going into lockdown`);
+			ui.log(`${channel}: RAID DETECTED, going into lockdown`);
+			ui.setStatus(`LOCKDOWN`);
 			if (!testMode) {
 				if (config.subsOnly) client.subscribers(channel); // If subs only is set, turn on subs only.
 				else client.followersonly(channel, config.followersOnlyAge); // Otherwise, turn on followers only.
 				client.r9kbeta(channel); // Turn on unique chat
 			} else {
-				if (config.subsOnly) console.info(`${channel}: TESTMODE - would set Subs only mode`);
-				else console.info(`${channel}: TESTMODE - would set Followers only mode for ${config.followersOnlyAge}`);
+				if (config.subsOnly) ui.log(`${channel}: TESTMODE - would set Subs only mode`);
+				else ui.log(`${channel}: TESTMODE - would set Followers only mode for ${config.followersOnlyAge}`);
 			}
 		}
 		lockdown = Date.now();
@@ -227,7 +239,7 @@ client.on('message', (channel, tags, message, self) => {
 				if (!userIDs.includes(val.tags.username) && !actedOnThisRaid.includes(val.tags.username)) userIDs.push(val.tags.username);
 			}
 		});
-		console.info(`${channel}: RAID MEMBERS: ${userIDs.join()}`);
+		//ui.log(`${channel}: RAID MEMBERS: ${userIDs.join()}`);
 		if (!testMode) {
 			if (!config.timeoutRaider && !config.banRaider) {
 				// We're not timing out or banning anyone, so just delete the messages.
@@ -247,17 +259,19 @@ client.on('message', (channel, tags, message, self) => {
 			if (!config.timeoutRaider && !config.banRaider) {
 				// We're not timing out or banning anyone, so just delete the messages.
 				messageIDs.forEach((id) => {
-					console.info(`${channel}: TESTMODE - would delete message ${id}`);
+					ui.log(`${channel}: TESTMODE - would delete message ${id}`);
 				});
 			} else {
 				// We're going to timeout or ban the user, which will purge the history anyway. So no need to delete.
 				userIDs.forEach((id) => {
 					// And timeout or ban the user that said them as above.
-					if (config.timeoutRaider) console.info(`${channel}: TESTMODE - would timeout ${id} for ${config.timeoutLength}, with reason ${config.reason}`);
-					if (config.banRaider) console.info(`${channel}: TESTMODE - would ban ${id} with reason ${config.reason}`);
+					if (config.timeoutRaider) ui.log(`${channel}: TESTMODE - would timeout ${id} for ${config.timeoutLength}, with reason ${config.reason}`);
+					if (config.banRaider) ui.log(`${channel}: TESTMODE - would ban ${id} with reason ${config.reason}`);
+					actedOnThisRaid.push(id);
 				});
 			}
 		}
+		ui.setRaiders(actedOnThisRaid);
 		rollingArray.deleteIDs(matchingMessages.map((elem) => elem.tags.id));
 	}
 	rollingArray.addEntry(tags, message);
